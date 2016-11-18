34e27c463a24b7fc293d94256d6bf2d6c6616da1
Satchel Grant <grantsrb@gmail.com>
2016-11-18 00:35:04 -0800

    add list and title to ResultsActivity

--- app/src/main/java/com/example/satchelgrant/docsap/ResultsActivity.java
+++ app/src/main/java/com/example/satchelgrant/docsap/ResultsActivity.java
@@ -1,13 +1,43 @@
 package com.example.satchelgrant.docsap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ResultsActivity extends AppCompatActivity {
+    private final String[] doctors = {"Dr. Osborn", "Dr. Dre", "Dr. Love", "Dr. DooLittle", "Dr. Octopus", "Dr. Metropolis"};
+    @Bind(R.id.doctorList) ListView mDoctorList;
+    @Bind(R.id.submittedSearch) TextView mSubmittedSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String search = intent.getStringExtra("search");
+
+        mSubmittedSearch.setText("Results for " + search);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, doctors);
+        mDoctorList.setAdapter(adapter);
+
+        mDoctorList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ResultsActivity.this, "Functionality to be added soon!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
