85b2402c83d12ccf6e7903369ba3a4440b6fca70
Satchel Grant <grantsrb@gmail.com>
2016-11-18 00:13:46 -0800

    add results page without contents

--- app/src/main/java/com/example/satchelgrant/docsap/SplashActivity.java
+++ app/src/main/java/com/example/satchelgrant/docsap/SplashActivity.java
@@ -3,13 +3,18 @@ package com.example.satchelgrant.docsap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.usernameDisplay) TextView mUserDisplay;
+    @Bind(R.id.submitSearch) Button mSubmitSearch;
+    @Bind(R.id.search) EditText mSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,17 @@ public class SplashActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         mUserDisplay.setText(intent.getStringExtra("username"));
+        mSubmitSearch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSubmitSearch) {
+            String searchText = mSearch.getText().toString();
+            searchText = searchText.equals("") ? searchText : "No Search Terms";
+            Intent newIntent = new Intent(SplashActivity.this, ResultsActivity.class);
+            newIntent.putExtra("search", searchText);
+            startActivity(newIntent);
+        }
     }
 }
